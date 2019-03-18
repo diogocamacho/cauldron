86034626153232bce8c62b6662cc67b4df11e9aa
Diogo Camacho <diogocamacho@mordor.fios-router.home>
2019-03-17 23:00:47 -0400

    - adding documentation - removing unncessary files

--- R/lincs_data.R
+++
@@ -1,6 +0,0 @@
-#' LINCS L1000 for DRUID
-#' 
-#' This data set was built based on the LINCS L1000 data, where each row represents a drug profile and columns correspond to genes.
-#' 
-#' 
-"LINCS_L1000"
